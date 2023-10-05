d1f96eb3845bb06dbca939163121d4e0dac74309
Kevin <staffordkevinjames@gmail.com>
2023-10-04 23:27:14 -0400

    ADD: Site Header and subHeader text

---
+++ portfolio-app/src/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header() {
+    return (
+    <header className="header">
+        <h1 id="head">Kevin J Stafford</h1>
+        <em id="subheader">Software Engineer Portfolio</em>
+    </header>
+    )
+}
+export default Header
